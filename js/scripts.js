0ae0db45d8c141126ebb3285ab1146bccec2c269
James Lee and Steve Burton <steve@>
2016-10-26 16:42:23 -0700

    trying to split a string into an array

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,30 @@
-var sentenceSplit = sentence.split("");
+//var makeArray = function(sentence) {
+  var sentenceSplit = sentence.split("");
+
+console.log(sentenceSplit);
+  //return sentenceSplit;
+
+  var finalSentence = sentenceSplit.join();
+//};
+
 var vowels = ["a", "e", "i", "o", "u"]
 
+var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"];
 
-sentenceSplit.forEach(vowels(sentenceSplit[0]))
 
-for(var index = 0; index < index[1]; index += 1) {
-  var pigLatin = function(sentence) {
-  if(sentence === "a" || sentence === "e" || sentence === "i" || sentence === "o" || sentence === "u") {
-    return sentence.concat("ay");
-    }
-  }
-};
+//for(var index = 0; index < index[1]; index += 1) {
+
+//}
+
+    var pigLatin = function(sentence) {
+    if(sentence === "a" || sentence === "e" || sentence === "i" || sentence === "o" || sentence === "u") {
+      return sentence.concat("ay");
+      }
+      else {
+        return sentence.concat("ay");
+      }
+
+ };
 
 
 $(document).ready(function() {
